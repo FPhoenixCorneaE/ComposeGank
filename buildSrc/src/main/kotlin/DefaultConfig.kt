21d95097a5223b7618c8b0c41a459ff1e128c842
wangkz <wangkz@infore.com>
2021-09-22 17:31:40 +0800

    build:initial commit;

---
+++ buildSrc/src/main/kotlin/DefaultConfig.kt
@@ -0,0 +1,8 @@
+object DefaultConfig {
+    const val applicationId = "com.fphoenixcorneae.gank.compose"
+    const val compileSdkVersion = 30
+    const val minSdkVersion = 21
+    const val targetSdkVersion = 30
+    const val versionCode = 100
+    const val versionName = "1.0.0"
+}
